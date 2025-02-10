2f985818fed83c5390094e90889e03105d749709
TempUserIdk18 <148344080+TempUserIdk18@users.noreply.github.com>
2025-02-10 15:08:29 +0500

    Update OreoLibSource.lua

--- OreoLibSource.lua
+++ OreoLibSource.lua
@@ -665,14 +665,6 @@ function OrionLib:MakeWindow(WindowConfig)
 		--	ImageColor3 = Color3.fromRGB(33, 33, 33),
 		--	ImageTransparency = 0.7
 		--}
-		local PoweredByLabel = AddThemeObject(SetProps(MakeElement("Label", "Powered by Oreo Lib", 14), {
-    Size = UDim2.new(0, 200, 0, 20), -- Adjust size as needed
-    Position = UDim2.new(0, 10, 1, -30), -- Bottom left corner
-    Font = Enum.Font.GothamBold,
-    TextXAlignment = Enum.TextXAlignment.Left
-}), "Text")
-
-PoweredByLabel.Parent = MainWindow
 
 		SetChildren(SetProps(MakeElement("TFrame"), {
 			Size = UDim2.new(1, 0, 0, 50),
@@ -783,6 +775,17 @@ PoweredByLabel.Parent = MainWindow
 	end	
 
 	local TabFunction = {}
+
+	local PoweredByLabel = AddThemeObject(SetProps(MakeElement("Label", "Powered by Oreo Lib", 14), {
+        Size = UDim2.new(0, 200, 0, 20), -- Adjust size if needed
+        Position = UDim2.new(0, 10, 1, -25), -- Bottom left corner
+        Font = Enum.Font.GothamBold,
+        TextXAlignment = Enum.TextXAlignment.Left,
+        BackgroundTransparency = 1 -- Ensure no background interference
+    }), "Text")
+
+    PoweredByLabel.Parent = MainWindow
+
         function TabFunction:SetFont(font)
     for _, v in pairs(MainWindow:GetDescendants()) do
         if v:IsA("TextLabel") or v:IsA("TextButton") then
