e74b9244e57df2fa430dbab600a952ddc214cb86
TempUserIdk18 <148344080+TempUserIdk18@users.noreply.github.com>
2025-02-10 15:12:46 +0500

    Update OreoLibSource.lua

--- OreoLibSource.lua
+++ OreoLibSource.lua
@@ -610,9 +610,6 @@ function OrionLib:MakeWindow(WindowConfig)
 				Size = UDim2.new(0, 32, 0, 32),
 				Position = UDim2.new(0, 10, 0.5, 0)
 			}), {
-				SetProps(MakeElement("Image", "https://www.roblox.com/headshot-thumbnail/image?userId=".. LocalPlayer.UserId .."&width=420&height=420&format=png"), {
-					Size = UDim2.new(1, 0, 1, 0)
-				}),
 				AddThemeObject(SetProps(MakeElement("Image", "rbxassetid://4031889928"), {
 					Size = UDim2.new(1, 0, 1, 0),
 				}), "Second"),
@@ -625,18 +622,7 @@ function OrionLib:MakeWindow(WindowConfig)
 			}), {
 				AddThemeObject(MakeElement("Stroke"), "Stroke"),
 				MakeElement("Corner", 1)
-			}),
-			AddThemeObject(SetProps(MakeElement("Label", LocalPlayer.DisplayName, WindowConfig.HidePremium and 14 or 13), {
-				Size = UDim2.new(1, -60, 0, 13),
-				Position = WindowConfig.HidePremium and UDim2.new(0, 50, 0, 19) or UDim2.new(0, 50, 0, 12),
-				Font = Enum.Font.GothamBold,
-				ClipsDescendants = true
-			}), "Text"),
-			AddThemeObject(SetProps(MakeElement("Label", "", 12), {
-				Size = UDim2.new(1, -60, 0, 12),
-				Position = UDim2.new(0, 50, 1, -25),
-				Visible = not WindowConfig.HidePremium
-			}), "TextDark")
+			})
 		}),
 	}), "Second")
 
@@ -778,7 +764,7 @@ function OrionLib:MakeWindow(WindowConfig)
 
 	local PoweredByLabel = AddThemeObject(SetProps(MakeElement("Label", "Powered by Oreo Lib", 14), {
         Size = UDim2.new(0, 200, 0, 20), -- Adjust size if needed
-        Position = UDim2.new(0, 10, 1, -25), -- Bottom left corner
+        Position = UDim2.new(0, 10, 1, -23), -- Bottom left corner
         Font = Enum.Font.GothamBold,
         TextXAlignment = Enum.TextXAlignment.Left,
         BackgroundTransparency = 1 -- Ensure no background interference
