904c338d08d26e26dcfc2a86e3b3d798eaa733be
TempUserIdk18 <148344080+TempUserIdk18@users.noreply.github.com>
2025-02-10 15:33:28 +0500

    Update OreoLibSource.lua

--- OreoLibSource.lua
+++ OreoLibSource.lua
@@ -747,7 +747,7 @@ function OrionLib:MakeWindow(WindowConfig)
 
 	local PoweredByLabel = AddThemeObject(SetProps(MakeElement("Label", "Oreo Lib", 14), {
         Size = UDim2.new(0, 200, 0, 20), -- Adjust size if needed
-        Position = UDim2.new(0, 10, 1, -45), -- Bottom left corner
+        Position = UDim2.new(0, 10, 1, -50), -- Bottom left corner
         Font = Enum.Font.GothamBold,
         TextXAlignment = Enum.TextXAlignment.Left,
         BackgroundTransparency = 1 -- Ensure no background interference
